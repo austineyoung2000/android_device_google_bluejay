97bb3f9e368493c59f0b88ef967155f1decf99b5
Austin Young <austineyoung2000@gmail.com>
2024-08-26 22:29:45 -0400

    bluejay: general upkeep
    * Nuked HBM
    * Added Pixel Parts

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,9 +15,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
-# HBM
-PRODUCT_PACKAGES := \
-    HbmSVManagerOverlay
+# PixelParts
+-include packages/apps/PixelParts/device.mk
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/bluejay-kernel/Image.lz4
