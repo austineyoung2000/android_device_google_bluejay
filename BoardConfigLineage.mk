11176a04bfdd02f0fd2bd5f7e3ab9e04e35fb9ed
Austin Young <austineyoung2000@gmail.com>
2025-01-22 08:58:46 -0500

    bluejay: Override system properties

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_DUP_RULES := true
+DEVICE_PATH := device/google/bluejay
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_bluejay
+
 # Kernel modules
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_boot
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_boot))
