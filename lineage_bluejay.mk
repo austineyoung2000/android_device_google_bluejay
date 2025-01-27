702f06c09556fc7d72dbdcca4b20e44b525a8bd7
Austin Young <austineyoung2000@gmail.com>
2025-01-26 21:40:38 -0500

    bluejay: general fixes for build time
    Change-Id: Ib4180594e8d77558273b42d8c8bc94ba065ebeef

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -43,9 +43,6 @@ TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 TARGET_ENABLE_BLUR := true
 
 RISING_MAINTAINER=EliteDarkKaiser
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RISING_CHIPSET="Google Tensor" \
     
 # Use Scudo instead of Jemalloc
 PRODUCT_USE_SCUDO := true
